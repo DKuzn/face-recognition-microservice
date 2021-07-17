16181b5848fe2fc4d4f4af6db1431dcbb1096d11
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2021-07-17 18:15:15 +0300

    Add classes to work with database
    Added classes:
     - Face for table "faces" that keeps referent features;
     - AvgFace for table "avg_face" that keeps average
    features of faces database faces;
     - PersonInfo for table "person_info" that keeps person
    info.

---
+++ FRMS/database.py
@@ -0,0 +1,81 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import torch
+import os
+
+
+DATABASE_URL = os.environ['DATABASE_URL'].replace('postgres', 'postgresql')
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+
+Base = declarative_base()
+
+
+class Face(Base):
+    __tablename__ = 'faces'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    features = Column(String)
+    distance = Column(Float)
+    person_id = Column(Integer, ForeignKey('person_info.id'))
+
+    def __init__(self, tensor: torch.Tensor, distance: float, person_id: int):
+        self.features = self._tensor_to_str(tensor)
+        self.distance = distance
+        self.person_id = person_id
+
+    @property
+    def tensor(self):
+        return torch.tensor(list(map(float, self.features.split(' '))))
+
+    @staticmethod
+    def _tensor_to_str(t):
+        return ' '.join([str(i) for i in t.tolist()])
+
+    def __repr__(self):
+        return "<Face('%s','%s','%s')>" % (self.features, self.distance, self.person_id)
+
+
+class AvgFace(Base):
+    __tablename__ = 'avg_face'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    features = Column(String)
+    faces_count = Column(Integer)
+
+    def __init__(self, tensor: torch.Tensor, faces_count: int):
+        self.features = self._tensor_to_str(tensor)
+        self.faces_count = faces_count
+
+    @property
+    def tensor(self):
+        return torch.tensor(list(map(float, self.features.split(' '))))
+
+    @staticmethod
+    def _tensor_to_str(t):
+        return ' '.join([str(i) for i in t.tolist()])
+
+    def __repr__(self):
+        return "<AvgFace('%s','%s')>" % (self.features, self.faces_count)
+
+
+class PersonInfo(Base):
+    __tablename__ = 'person_info'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    surname = Column(String)
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+    def __repr__(self):
+        return "<PersonInfo('%s','%s')>" % (self.name, self.surname)
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    users_table = Face.__tablename__, PersonInfo.__tablename__, AvgFace.__tablename__
+    print(users_table)
+    metadata = Base.metadata
+    print(metadata)
