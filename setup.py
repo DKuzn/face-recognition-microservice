f70e249f4e1447e8a057a681af0897929fc597f9
Dmitry Kuznetsov <DKuznetsov2000@mail.ru>
2021-08-03 13:01:37 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from FRMS import __version__
+
+setup(
+    name='FRMS',
+    version=__version__,
+    packages=['FRMS', 'FRMS.utils'],
+    url='https://github.com/DKuzn/FaceRecognitionMicroservice',
+    license='LGPLv3',
+    author='Dmitry Kuznetsov',
+    author_email='DKuznetsov2000@mail.ru',
+    description='Microservice for face recognition',
+    install_requires=['fastapi', 'torch', 'torchvision', 'facenet_pytorch', 'SQLAlchemy', 'psycopg2-binary', 'uvicorn']
+)
