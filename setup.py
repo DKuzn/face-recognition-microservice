d98ee3457ddd9440db5b5b15c8fdecfbee34dcea
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2021-11-01 13:45:13 +0300

    Update setup.py
    Added package mysqlclient to require installs.
    
    Updated URL to the project repository.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='FRMS',
     version=__version__,
     packages=['FRMS', 'FRMS.utils'],
-    url='https://github.com/DKuzn/FaceRecognitionMicroservice',
+    url='https://github.com/DKuzn/face-recognition-microservice',
     license='LGPLv3',
     author='Dmitry Kuznetsov',
     author_email='DKuznetsov2000@outlook.com',
     description='Microservice for face recognition',
-    install_requires=['fastapi', 'torch', 'torchvision', 'facenet_pytorch', 'SQLAlchemy', 'psycopg2-binary', 'uvicorn']
+    install_requires=['fastapi', 'torch', 'torchvision', 'facenet_pytorch', 'SQLAlchemy', 'psycopg2-binary', 'mysqlclient', 'uvicorn']
 )
