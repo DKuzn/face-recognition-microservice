5d23e803c5f1e5415e4b12a30a78d99385f11727
Dmitry Kuznetsov <DKuznetsov2000@outlook.com>
2022-06-01 18:45:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version=__version__,
     packages=['FRMS', 'FRMS.utils'],
     url='https://github.com/DKuzn/face-recognition-microservice',
-    license='MIT',
+    license='GPLv3',
     author='Dmitry Kuznetsov',
     author_email='DKuznetsov2000@outlook.com',
     description='Microservice for face recognition',
